15131c47747100340fa0825d54770025e3b4dc46
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-02-19 23:57:59 +0200

    Setup Navigation between Fragments

--- app/src/main/java/com/example/trackingapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/trackingapp/ui/MainActivity.kt
@@ -2,13 +2,37 @@ package com.example.trackingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.trackingapp.R
+import com.example.trackingapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.rootView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setSupportActionBar(binding.toolbar)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        // To Control the Visibility of the BottomNavigationView on certain Fragments
+        navController
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment , R.id.runFragment , R.id.statisticsFragment -> {
+                        binding.bottomNavigationView.visibility = View.VISIBLE
+                    }
+                    else -> binding.bottomNavigationView.visibility = View.GONE
+                }
+
+            }
     }
 }
--- app/src/main/java/com/example/trackingapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/trackingapp/ui/fragments/RunFragment.kt
@@ -6,11 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.trackingapp.R
+import com.example.trackingapp.databinding.FragmentRunBinding
 import com.example.trackingapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
+    private lateinit var binding: FragmentRunBinding
     private val viewModel: MainViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRunBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment2_to_trackingFragment)
+        }
+    }
 }
